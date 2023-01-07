7efe32101bbb96dfc8af401d0b26e42e5e7bb800
yactouat <yactouat@hotmail.com>
2023-01-07 17:27:25 +0100

    converted codebase to Typescript

---
+++ src/index.ts
@@ -0,0 +1,223 @@
+/**
+ *
+ * checks if input nb1 is a factor of input nb2
+ *
+ * @param {number} nb1 the number divisor
+ * @param {number} nb2 the possible multiple to check
+ * @return {boolean} returns true if nb1 is a factor of nb2
+ *
+ */
+export const isFactor = (nb1: number, nb2: number): boolean => {
+  // 0 cannot be a factor of any number except for 0
+  if (nb2 === 0 && nb1 === 0) {
+    return true;
+  }
+  return (
+    Number.isInteger(nb1) && Number.isInteger(nb2) && nb1 > 0 && nb2 % nb1 === 0
+  );
+};
+
+/**
+ *
+ * checks if a number is an even number
+ *
+ * @param {number} nb the number to check
+ * @return {boolean} if the number is an even number
+ *
+ */
+export const isEven = (nb: number): boolean => {
+  return isFactor(2, nb);
+};
+
+/**
+ *
+ * checks if input nb1 is a multiple of input nb2
+ *
+ * @param {number} nb1 the possible multiple to check
+ * @param {number} nb2 the possible factor to check
+ * @return {boolean} returns true if nb1 is a multiple of nb2
+ *
+ */
+export const isMultiple = (nb1: number, nb2: number): boolean => {
+  return isFactor(nb2, nb1);
+};
+
+/**
+ *
+ * checks if a number is a natural number
+ *
+ * a natural number is any positive integer > 0
+ *
+ * @param {number} nb the number to check
+ * @return {boolean} if the number is a natural number
+ *
+ */
+export const isNaturalNumber = (nb: number): boolean => {
+  return nb > 0 && Number.isInteger(nb);
+};
+
+/**
+ *
+ * checks if a number is a prime number,
+ *
+ * a prime number is a natural number that has exactly two factors,
+ * meaning it is only divisible by 1 and itself
+ *
+ * @param {number} nb the number to check, will return false if no number
+ * @return {boolean} if the number is a natural number
+ *
+ */
+export const isPrime = (nb: number): boolean => {
+  if (isNaturalNumber(nb)) {
+    // 1 is not a prime number
+    if (nb === 1) return false;
+    // every even number that is not 2 is not prime
+    if (nb > 2 && isEven(nb)) return false;
+    /**
+     *  if nb is a multiple of 5, it is not prime
+     *  we add this common check for performance reason because it helps us avoid some loop iterations
+     * */
+    if (nb > 5 && isMultiple(nb, 5)) return false;
+    /**
+     *  every natural number has at least 2 factors, 1 and itself
+     *  this is why we start the counter at 2 in the below loop
+     * */
+    let numberOfFactors = 2;
+    for (let i = 2; i < nb; i++) {
+      // there's no need to continue the loop if we already have more than 2 factors
+      if (numberOfFactors > 2) break;
+      if (isFactor(i, nb)) {
+        numberOfFactors++;
+      }
+    }
+    // checking primality by assessing that number of factors is still 2 after the loop
+    return numberOfFactors === 2;
+  }
+  return false;
+};
+
+/**
+ *
+ * TODO test
+ *
+ * gets a string representation, in hours and minutes, of a decimal number
+ *
+ * @param {number} timeInDecimal
+ *
+ * @return {string} the hours:minutes string representation of the input decimal number
+ * @throws "Input has to be a positive number !"
+ *
+ */
+export const getDecimalToHoursAndMinutes = (timeInDecimal: number) => {
+  if (timeInDecimal < 0) {
+    throw "Input has to be a positive number !";
+  }
+  let hours = Math.floor(timeInDecimal);
+  let hoursStr = Math.floor(timeInDecimal).toString();
+  let minutes = Math.round((timeInDecimal - hours) * 60);
+  let minutesStr = minutes.toString();
+  if (hours < 10) hoursStr = "0" + hours;
+  if (minutes < 10) minutesStr = "0" + minutes;
+  return hours + ":" + minutes;
+};
+
+/**
+ *
+ * gets what percentage of nb2 represents nb1;
+ *
+ * rounded to 2 decimals for readability
+ *
+ * @param {number} nb1 the part
+ * @param {number} nb2 the whole
+ *
+ * @return {number} the percentage of nb2 represented by nb1
+ * @throws "Division by zero impossible !"
+ *
+ */
+export const getPercentageRepresentation = (
+  nb1: number,
+  nb2: number
+): number => {
+  if (nb2 == 0) throw "Division by zero impossible !";
+  return Number(((nb1 / nb2) * 100).toFixed(2));
+};
+
+/**
+ *
+ * @function getPrimeFactorization
+ *
+ * @summary gets a list of prime factors for the given input value
+ *
+ * @param {number} nb the number to check
+ * @return {Array} an array containing the list of the prime factors, their product is equal to the input nb
+ *
+ */
+export const getPrimeFactorization = (nb: number): number[] => {
+  let result: number[] = [];
+  if (isNaturalNumber(nb) && nb > 1) {
+    if (isPrime(nb)) {
+      return [nb];
+    }
+    // we get the list of unique prime factors for the input nb
+    let uniquePrimeFactorsList = getUniquePrimeFactors(nb);
+    // we initialize our result array
+    result = [uniquePrimeFactorsList[0]];
+    // we divide the input nb by the first prime factor of the list
+    let quotient = nb / uniquePrimeFactorsList[0];
+    /**
+     *
+     * while the quotient is not a prime,
+     * we add its first prime factor to the result,
+     * and we reassign the quotient to its division by this prime factor
+     *
+     * */
+    while (!isPrime(quotient)) {
+      const firstQuotientPrimeFactor = getUniquePrimeFactors(quotient)[0];
+      result.push(firstQuotientPrimeFactor);
+      quotient = quotient / firstQuotientPrimeFactor;
+    }
+    // if the quotient is a prime we add it to our result
+    if (isPrime(quotient)) result.push(quotient);
+  }
+  return result;
+};
+
+/**
+ *
+ * @function getUniquePrimeFactors
+ *
+ * @summary returns a list of unique prime factors for the given input value
+ *
+ * @param {number} nb the number to check
+ * @return {Array} an array containing the list of the prime factors
+ *
+ */
+export const getUniquePrimeFactors = (nb: number): number[] => {
+  let results = [];
+  if (isPrime(nb)) results.push(nb);
+  // we start the loop with the first prime number that we know of
+  for (let i = 2; i < nb; i++) {
+    // push to array of results if iterated number is a factor of the nb and is prime
+    if (isPrime(i) && isFactor(i, nb)) results.push(i);
+  }
+  return results;
+};
+
+/**
+ *
+ * returns how many items there is for one nb1 unit per nb2 unit,
+ *
+ * as in "for 1 unit of nb1, there are n units of nb2"
+ *
+ * @param {number} nb1 the first number to compare
+ * @param {number} nb2 the second number to compare
+ * @return {string} the ratio expressed in terms of '1:n', rounded to 2 decimals
+ *
+ * @throws "No ratio possible when left hand value is zero"
+ *
+ */
+export const getUnitRatioOfNb1ToNb2 = (nb1: number, nb2: number): string => {
+  if (nb1 === 0) throw "No ratio possible when left hand value is zero";
+  if (nb2 === 0) return "1:0.00";
+  return `1:${Number(nb2 / nb1).toFixed(2)}`;
+};
---
+++ tests/index.test.ts
@@ -0,0 +1,365 @@
+const jestTheories = require("jest-theories");
+import {
+  getPercentageRepresentation,
+  getPrimeFactorization,
+  getUniquePrimeFactors,
+  getUnitRatioOfNb1ToNb2,
+  isEven,
+  isMultiple,
+  isNaturalNumber,
+  isPrime,
+} from "./../src/index";
+
+describe("value is a multiple", () => {
+  const theories = [
+    { input: 64, input2: 32, expected: true },
+    { input: 0, input2: 0, expected: true },
+    { input: 0, input2: 1124123, expected: true },
+    { input: 1, input2: 0, expected: false },
+    { input: 1.3, input2: 1, expected: false },
+    { input: 500, input2: 5, expected: true },
+    { input: -4, input2: -2, expected: false },
+    { input: -2, input2: 2, expected: true },
+    { input: 2, input2: 4, expected: false },
+  ];
+  jestTheories.default(
+    "the value {input} is a multiple of {input2} => {expected}",
+    theories,
+    (theory: { input: number; input2: number; expected: boolean }) => {
+      expect(isMultiple(theory.input as number, theory.input2 as number)).toBe(
+        theory.expected
+      );
+    }
+  );
+});
+
+describe("value is even", () => {
+  const theories = [
+    // check out https://www.bbc.com/news/magazine-20559052 if you have a doubt about 0 being even
+    { input: 0, expected: true },
+    { input: 1, expected: false },
+    { input: 1.3, expected: false },
+    { input: 500, expected: true },
+    { input: 2, expected: true },
+    { input: 66, expected: true },
+  ];
+  jestTheories.default(
+    "the value {input} is even => {expected}",
+    theories,
+    (theory: { input: number; expected: boolean }) => {
+      expect(isEven(theory.input as number)).toBe(theory.expected);
+    }
+  );
+});
+
+describe("value is a natural number", () => {
+  const theories = [
+    { input: 0, expected: false },
+    { input: 1, expected: true },
+    { input: 1.3, expected: false },
+    { input: 500, expected: true },
+    { input: -2, expected: false },
+  ];
+  jestTheories.default(
+    "the value {input} is a natural number => {expected}",
+    theories,
+    (theory: { input: number; expected: boolean }) => {
+      expect(isNaturalNumber(theory.input as number)).toBe(theory.expected);
+    }
+  );
+});
+
+describe("number is prime", () => {
+  const theories = [
+    { input: -3, expected: false },
+    { input: 0, expected: false },
+    { input: 1, expected: false },
+    { input: 2, expected: true },
+    { input: 3, expected: true },
+    { input: 5, expected: true },
+    { input: 11, expected: true },
+    { input: 15, expected: false },
+    { input: 20, expected: false },
+    { input: 100, expected: false },
+    { input: 45, expected: false },
+    { input: 63, expected: false },
+    { input: 73, expected: true },
+    { input: 19, expected: true },
+    { input: 7, expected: true },
+    { input: 5, expected: true },
+    { input: 71, expected: true },
+    { input: 17, expected: true },
+    { input: 29, expected: true },
+    { input: 41, expected: true },
+    { input: 97, expected: true },
+    { input: 47, expected: true },
+    { input: 31, expected: true },
+    { input: 11, expected: true },
+    { input: 13, expected: true },
+    // testing performance and loop iterations skipping with multiples of 5 on a very large number
+    { input: 949849845316494994989811984984983, expected: false },
+    { input: 949849845316494994989811984984980, expected: false },
+  ];
+  jestTheories.default(
+    "the number {input} is a prime number => {expected}",
+    theories,
+    (theory: { input: number; expected: boolean }) => {
+      expect(isPrime(theory.input as number)).toBe(theory.expected);
+    }
+  );
+});
+
+describe("prime factorization", () => {
+  const theories = [
+    { input: -3, expected: [] },
+    { input: -18, expected: [] },
+    { input: 0, expected: [] },
+    { input: 1, expected: [] },
+    { input: 1.35, expected: [] },
+    { input: 25.899, expected: [] },
+    { input: 75, expected: [3, 5, 5] },
+    { input: 36, expected: [2, 2, 3, 3] },
+    { input: 30, expected: [2, 3, 5] },
+    { input: 73, expected: [73] },
+    { input: 126, expected: [2, 3, 3, 7] },
+    { input: 12, expected: [2, 2, 3] },
+    { input: 18, expected: [2, 3, 3] },
+    { input: 24, expected: [2, 2, 2, 3] },
+  ];
+  jestTheories.default(
+    "the prime factorization of {input} is {expected}",
+    theories,
+    (theory: { input: number; expected: number[] }) => {
+      expect(getPrimeFactorization(theory.input as number)).toStrictEqual(
+        theory.expected
+      );
+    }
+  );
+});
+
+describe("prime factorization unique values", () => {
+  const theories = [
+    { input: -3, expected: [] },
+    { input: -18, expected: [] },
+    { input: 0, expected: [] },
+    { input: 1, expected: [] },
+    { input: 1.35, expected: [] },
+    { input: 25.899, expected: [] },
+    { input: 75, expected: [3, 5] },
+    { input: 36, expected: [2, 3] },
+    { input: 30, expected: [2, 3, 5] },
+    { input: 73, expected: [73] },
+    { input: 126, expected: [2, 3, 7] },
+    { input: 12, expected: [2, 3] },
+    { input: 18, expected: [2, 3] },
+    { input: 24, expected: [2, 3] },
+  ];
+  jestTheories.default(
+    "the unique prime factors of {input} are {expected}",
+    theories,
+    (theory: { input: number; expected: number[] }) => {
+      expect(getUniquePrimeFactors(theory.input as number)).toStrictEqual(
+        theory.expected
+      );
+    }
+  );
+});
+
+describe("value of what percentage of nb2 represents nb1", () => {
+  const theories = [
+    { input: [-1, 2], expected: -50 },
+    { input: [9, -18], expected: -50 },
+    { input: [1, 1.35], expected: 74.07 },
+    { input: [25.899, 5], expected: 517.98 },
+    { input: [75, 100], expected: 75 },
+    { input: [6, 36], expected: 16.67 },
+    { input: [5, 30], expected: 16.67 },
+    { input: [3, 73], expected: 4.11 },
+    { input: [12, 126], expected: 9.52 },
+    { input: [3, 12], expected: 25 },
+    { input: [2, 18], expected: 11.11 },
+    { input: [24, 12], expected: 200 },
+  ];
+  jestTheories.default(
+    "{input} ratio is {expected}",
+    theories,
+    (theory: { input: [number, number]; expected: number }) => {
+      expect(
+        getPercentageRepresentation(
+          theory.input[0] as number,
+          theory.input[1] as number
+        )
+      ).toStrictEqual(theory.expected);
+    }
+  );
+
+  test("cant get a percentage out of 0", () => {
+    const getPercentageOutOfZero = () => {
+      getPercentageRepresentation(5, 0);
+    };
+    expect(getPercentageOutOfZero).toThrowError(
+      "Division by zero impossible !"
+    );
+  });
+
+  test("cant pass an expression as nb2", () => {
+    expect(getPercentageRepresentation(50, 10 * 10)).toBe(50);
+    expect(getPercentageRepresentation(99, 500 + 51)).toBe(17.97);
+  });
+});
+
+describe("ratio of 1 nb1 unit to n units of nb2", () => {
+  describe("ratio when nb1 is bigger than nb2", () => {
+    const theories = [
+      { input: [3, 2], expected: "1:0.67" },
+      { input: [24, 4], expected: "1:0.17" },
+      { input: [24, 3], expected: "1:0.13" },
+    ];
+    jestTheories.default(
+      "ratio between {input} is {expected} for 1 unit of nb1",
+      theories,
+      (theory: { input: [number, number]; expected: string }) => {
+        expect(
+          getUnitRatioOfNb1ToNb2(
+            theory.input[0] as number,
+            theory.input[1] as number
+          )
+        ).toEqual(theory.expected);
+      }
+    );
+  });
+
+  describe("ratio when nb1 is smaller than nb2", () => {
+    const theories = [
+      { input: [3, 24], expected: "1:8.00" },
+      { input: [2, 3], expected: "1:1.50" },
+      { input: [4, 24], expected: "1:6.00" },
+    ];
+    jestTheories.default(
+      "ratio between {input} is {expected} for 1 unit of nb1",
+      theories,
+      (theory: { input: [number, number]; expected: string }) => {
+        expect(
+          getUnitRatioOfNb1ToNb2(
+            theory.input[0] as number,
+            theory.input[1] as number
+          )
+        ).toEqual(theory.expected);
+      }
+    );
+  });
+  describe("ratio when nb1 is smaller than 1", () => {
+    const theories = [
+      { input: [0.3, 24], expected: "1:80.00" },
+      { input: [0.2, 3], expected: "1:15.00" },
+      { input: [0.44, 24], expected: "1:54.55" },
+    ];
+    jestTheories.default(
+      "ratio between {input} is {expected} for 1 unit of nb1",
+      theories,
+      (theory: { input: [number, number]; expected: string }) => {
+        expect(
+          getUnitRatioOfNb1ToNb2(
+            theory.input[0] as number,
+            theory.input[1] as number
+          )
+        ).toEqual(theory.expected);
+      }
+    );
+  });
+
+  test("ratio when nb1 is equal to nb2", () => {
+    expect(getUnitRatioOfNb1ToNb2(2, 2)).toEqual("1:1.00");
+  });
+
+  test("ratio when nb1 is 0", () => {
+    const getRatioWhenNb1Is0 = () => {
+      getUnitRatioOfNb1ToNb2(0, 5);
+    };
+    expect(getRatioWhenNb1Is0).toThrowError(
+      "No ratio possible when left hand value is zero"
+    );
+  });
+
+  test("return value is a string formatted in terms of '1:n'", () => {
+    // arrange
+    const expected = "1:2.00";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(2, 4);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+
+  describe("return value right hand has no more than 2 decimals by default", () => {
+    const theories = [
+      { input: [2, 4], expected: 2 },
+      { input: [0.44, 24], expected: 2 },
+    ];
+    jestTheories.default(
+      "ratio number of decimals between {input} is {expected} for 1 unit of nb1 is expected to be 2",
+      theories,
+      (theory: { input: [number, number]; expected: number }) => {
+        expect(
+          getUnitRatioOfNb1ToNb2(
+            theory.input[0] as number,
+            theory.input[1] as number
+          ).split(".")[1].length
+        ).toEqual(theory.expected);
+      }
+    );
+  });
+
+  test("ratio when nb2 is 0", () => {
+    // arrange
+    const expected = "1:0.00";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(2, 0);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+
+  test("ratio when nb2 is a negative number and nb1 is positive", () => {
+    // arrange
+    const expected = "1:-1.00";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(2, -2);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+
+  test("ratio when nb1 is a negative number and nb2 is positive", () => {
+    // arrange
+    const expected = "1:-1.00";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(-2, 2);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+
+  test("ratio when both numbers are negative and nb1 is smaller", () => {
+    // arrange
+    const expected = "1:2.00";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(-2, -4);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+
+  test("ratio when both numbers are negative and nb1 is bigger", () => {
+    // arrange
+    const expected = "1:1.50";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(-4, -6);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+
+  test("ratio when both numbers are negative and equal to each other", () => {
+    // arrange
+    const expected = "1:1.00";
+    // act
+    const actual = getUnitRatioOfNb1ToNb2(-4, -4);
+    // assert
+    expect(actual).toEqual(expected);
+  });
+});
